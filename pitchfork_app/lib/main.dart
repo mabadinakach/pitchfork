825ff697025854c181c642cf40be4c44ad91ec25
mabadinakach <mabadinakach@gmail.com>
2021-03-17 13:21:44 -0600

    [+] styling + fixed bugs

--- pitchfork_app/lib/main.dart
+++ pitchfork_app/lib/main.dart
@@ -1,9 +1,11 @@
 import 'dart:convert';
 import 'dart:developer';
+import 'dart:math';
 
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:carousel_slider/carousel_slider.dart';
 
 void main() {
   runApp(MyApp());
@@ -53,11 +55,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
-  
-
-  
-
   void onTabTapped(int index) {
    setState(() {
      _currentIndex = index;
@@ -66,19 +63,16 @@ class _MyHomePageState extends State<MyHomePage> {
 
   int _currentIndex = 0;
 
-  
-
   final List<Widget> _children = [
     Search(),
     Explore()
-    
   ];
 
   @override
   Widget build(BuildContext context) {
    return Scaffold(
      appBar: AppBar(
-       title: Text('Pitchfork'),
+       title: Text('Pitchfork Client App'),
      ),
      body: _children[_currentIndex], // new
      bottomNavigationBar: BottomNavigationBar(
@@ -113,14 +107,21 @@ class _SearchState extends State<Search> {
   @override
   void initState() { 
     super.initState();
+    artist = "";
+    album = "";
   }
 
   Future getReview(String artist, String album) async {
     _showDialog();
     var url = Uri.parse("http://192.168.1.64:5000/a=$artist&b=$album");
     await http.get(url).then((response) {
+      if (response.statusCode != 200) {
+        Navigator.of(context).pop();
+        _showError("Error: ${response.statusCode}");
+      }
       var json1 = json.decode("[" + response.body + "]");
-
+      print(response.statusCode);
+      
       if (json1[0]["status"] == false) {
         Navigator.of(context).pop();
         _showError(json1[0]["message"]);
@@ -132,38 +133,25 @@ class _SearchState extends State<Search> {
         Navigator.of(context).pop();
         Navigator.of(context).push(MaterialPageRoute(builder: (context) => ReviewDetail(data)));
       }
+      
     }); 
   }
 
   void _showDialog() {
-    // flutter defined function
     showDialog(
       context: context,
       builder: (BuildContext context) {
-        // return object of type Dialog
         return AlertDialog(
-          //title: new Text("Alert Dialog title"),
-          content: Container(width: 50, height: 100,child: Image.network("https://c.tenor.com/HJvqN2i4Zs4AAAAj/milk-and-mocha-cute.gif")),
-          // actions: <Widget>[
-          //   // usually buttons at the bottom of the dialog
-          //   new TextButton(
-          //     child: new Text("Close"),
-          //     onPressed: () {
-          //       Navigator.of(context).pop();
-          //     },
-          //   ),
-          // ],
+          content: Container(width: 50, height: 100,child: Image.network("https://media3.giphy.com/media/gdTk7DyjblYWyqyBhG/giphy.gif")),
         );
       },
     );
   }
 
   void _showError(error) {
-    // flutter defined function
     showDialog(
       context: context,
       builder: (BuildContext context) {
-        // return object of type Dialog
         return AlertDialog(
           title: new Text("Error"),
           content: Text(error),
@@ -180,40 +168,116 @@ class _SearchState extends State<Search> {
     );
   }
 
+  List<Image> images = [
+    Image.network("https://images.squarespace-cdn.com/content/v1/5755a35501dbae3c6d1ba03e/1603208225015-DJ9F5QMIY8SL2HL1FK6S/ke17ZwdGBToddI8pDm48kB9VQfqtly6fKqkiYCkPXaFZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZUJFbgE-7XRK3dMEBRBhUpyn6gQ4NFdQ7R4MR6wxnXbxgJd6wf4utYaxP954dD4SXKzitVMbPRi_LQvaXUdzF4Y/Screen+Shot+2020-10-20+at+11.36.46+AM.png?format=750w"),
+    Image.network("https://images.squarespace-cdn.com/content/v1/5755a35501dbae3c6d1ba03e/1603208312230-K6CHZPQOCEFKI2LHDJKE/ke17ZwdGBToddI8pDm48kNb_7ypA_97UsF9m0cdu7zhZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZUJFbgE-7XRK3dMEBRBhUpzGdIKNtx6iBSa8b6m2ZOvcEYY1k593GJ5Dnhz7BHNowinbPXj6twav4Aph9JbyP-w/Screen+Shot+2020-10-20+at+11.38.19+AM.png?format=750w"),
+    Image.network("https://images.squarespace-cdn.com/content/v1/5755a35501dbae3c6d1ba03e/1603208565381-AY8FVBNGM6TMOMJPFIJW/ke17ZwdGBToddI8pDm48kLxMzUo65OKC-k7qJ3953dlZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZUJFbgE-7XRK3dMEBRBhUpyf0mNtCa6kd1EiReSwtkHLow6cvoYhSYjlzz29zCx-mdhztxC8JdfFZ2hWg6OE7k0/Screen+Shot+2020-10-20+at+11.42.27+AM.png?format=750w"),
+    Image.network("https://images.squarespace-cdn.com/content/v1/5755a35501dbae3c6d1ba03e/1603208885401-D1X1N2782M20BKNQYO2Q/ke17ZwdGBToddI8pDm48kLxMzUo65OKC-k7qJ3953dlZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZUJFbgE-7XRK3dMEBRBhUpyf0mNtCa6kd1EiReSwtkHLow6cvoYhSYjlzz29zCx-mdhztxC8JdfFZ2hWg6OE7k0/Screen+Shot+2020-10-20+at+11.43.39+AM.png?format=750w"),
+    Image.network("https://images.squarespace-cdn.com/content/v1/5755a35501dbae3c6d1ba03e/1603208945539-V7IT7YOD6ZRRF1RJBW2Y/ke17ZwdGBToddI8pDm48kLxMzUo65OKC-k7qJ3953dlZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZUJFbgE-7XRK3dMEBRBhUpyf0mNtCa6kd1EiReSwtkHLow6cvoYhSYjlzz29zCx-mdhztxC8JdfFZ2hWg6OE7k0/Screen+Shot+2020-10-20+at+11.48.51+AM.png?format=750w"),
+    Image.network("https://images.squarespace-cdn.com/content/v1/5755a35501dbae3c6d1ba03e/1603209010717-IMH38WH2XS0AOEK6ZZ8Z/ke17ZwdGBToddI8pDm48kPjeJAm00CDHWh_ZMgXjEWNZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZUJFbgE-7XRK3dMEBRBhUpz4rSyg3jA_cSuDPSO-0PpgqXrlfAT89Ia_69KV_pyRiZvZCzjgv3X8ZCe8_Y-5Jpw/Screen+Shot+2020-10-20+at+11.49.58+AM.png?format=750w"),
+    Image.network("https://images.squarespace-cdn.com/content/v1/5755a35501dbae3c6d1ba03e/1603211152010-DSPG8MGYQWRTSNSWGL5N/ke17ZwdGBToddI8pDm48kDHnLv2JTc2j110zwWiPDQ9Zw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZUJFbgE-7XRK3dMEBRBhUpx9MNbzgNsBi34wnReUFO4zdGl9QjvD6nb9gAQKsS-3xXYK4AuBCyLWKDff_EX6ut4/Screen+Shot+2020-10-20+at+12.25.26+PM.png?format=750w"),
+    Image.network("https://images.squarespace-cdn.com/content/v1/5755a35501dbae3c6d1ba03e/1603209107637-FDSOSBXTE0FF53R5WA5T/ke17ZwdGBToddI8pDm48kMOEKR6dMCPy4fM9-VkAFQpZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZUJFbgE-7XRK3dMEBRBhUpxOaa7bJC5nC-3Vx8_VvQU5ZHSi5EFCqkqDpHcCRO5IdflSV9hpbzW8q9sq3pWEROU/Screen+Shot+2020-10-20+at+11.51.01+AM.png?format=750w"),
+    Image.network("https://images.squarespace-cdn.com/content/v1/5755a35501dbae3c6d1ba03e/1603209273181-H1MG6JQO5C400ROPPYHC/ke17ZwdGBToddI8pDm48kDrMjE7hBq4fQV3wYHraitJZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZUJFbgE-7XRK3dMEBRBhUpyGCq5VpfeBJu-82OcOh-bqszTBJBXlZyh56jWduE-YAd2_jHuFGTcrhGEJySBGUHo/Screen+Shot+2020-10-20+at+11.53.09+AM.png?format=750w"),
+    Image.network("https://images.squarespace-cdn.com/content/v1/5755a35501dbae3c6d1ba03e/1603209367515-EFGPZ22X1ITJH8S81KGX/ke17ZwdGBToddI8pDm48kBhjaGRc9woxQsp0OESMfwNZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZUJFbgE-7XRK3dMEBRBhUpyngboOM180BszoBgc3fumCi_tcEB7kpB1QYLcJqR6kt4QKmzUY1LAfx_037DOA0w8/Screen+Shot+2020-10-20+at+11.55.40+AM.png?format=750w"),
+    Image.network("https://images.squarespace-cdn.com/content/v1/5755a35501dbae3c6d1ba03e/1603209430454-3BBG417O47EBIEYSTZ74/ke17ZwdGBToddI8pDm48kM8kgpbBhpuRIFgMPwjKfHZZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZUJFbgE-7XRK3dMEBRBhUpyYj3FJCo-0TwancYhkFrKhbSnTsBk3FrdMBYolUAriybe62lzI7vM675NepYfXY0w/Screen+Shot+2020-10-20+at+11.56.56+AM.png?format=750w"),
+    Image.network("https://images.squarespace-cdn.com/content/v1/5755a35501dbae3c6d1ba03e/1603209535664-LE9J2F5UJ9HPD7KJ0UJA/ke17ZwdGBToddI8pDm48kCiHm6FumI7xn1vYkvImj9ZZw-zPPgdn4jUwVcJE1ZvWEtT5uBSRWt4vQZAgTJucoTqqXjS3CfNDSuuf31e0tVHDxKMe0ejUNARF_HkkTy2UGvJE6LDZplt4FfGLYEhAK91lH3P2bFZvTItROhWrBJ0/Screen+Shot+2020-10-20+at+11.58.41+AM.png?format=500w"),
+    Image.network("https://images.squarespace-cdn.com/content/v1/5755a35501dbae3c6d1ba03e/1603209640186-7O4X5XNJXSZMFFEH0AIB/ke17ZwdGBToddI8pDm48kJCnqEJNa8cQx4dOK61i5M9Zw-zPPgdn4jUwVcJE1ZvWEtT5uBSRWt4vQZAgTJucoTqqXjS3CfNDSuuf31e0tVHutsJOOT2Y-AmV66IFlbAuCMhmZUNNPTGbVM5MYYp1-xur-lC0WofN0YB1wFg-ZW0/Screen+Shot+2020-10-20+at+12.00.23+PM.png?format=500w"),
+    Image.network("https://static.billboard.com/files/media/The-Velvet-Underground-Nico-greatest-album-covers-billboard-1000x1000-compressed.jpg"),
+    Image.network("https://static.billboard.com/files/media/The-Beatles-Abbey-Road-album-covers-billboard-1000x1000-compressed.jpg"),
+    Image.network("https://static.billboard.com/files/media/Patti-Smith-Horses-album-covers-billboard-1000x1000-compressed.jpg"),
+    Image.network("https://static.billboard.com/files/media/Led-Zeppelin-Led-Zeppelin-greatest-album-covers-billboard-1000x1000-compressed.jpg"),
+    Image.network("https://static.billboard.com/files/media/Pink-Floyd-Dark-Side-of-the-Moon-album-covers-billboard-1000x1000-compressed.jpg"),
+    Image.network("https://static.billboard.com/files/media/Nirvana-Nevermind-album-covers-billboard-1000x1000-compressed.jpg"),
+    Image.network("https://static.billboard.com/files/media/Cyndi-Lauper-Shes-So-Unusual-album-covers-billboard-1000x1000-compressed.jpg"),
+    Image.network("https://static.billboard.com/files/media/The-Beatles-Sgt-Peppers-lonely-hearts-club-band-album-covers-billboard-1000x1000-compressed.jpg"),
+    Image.network("https://static.billboard.com/files/media/To-pimp-a-butterfly-kendrick-lamar-no1-albums-billboard-1000x1000-compressed.jpg"),
+  ];
+
+  var rng = new Random();
+
+  FocusNode artistFocusNode;
+  FocusNode albumFocusNode;
+
   @override
   Widget build(BuildContext context) {
+    final node = FocusScope.of(context);
     return Scaffold(
-      body: Padding(
+      body: ListView.builder(
+        itemCount: 1,
+        itemBuilder: (BuildContext context, int index) {
+        return Padding(
         padding: const EdgeInsets.all(8.0),
         child: Column(
         children: [
-          SizedBox(height: 50,),
+          Container(
+            child: CarouselSlider.builder(
+              itemCount: images.length,
+              options: CarouselOptions(
+                aspectRatio: 1,
+                enlargeCenterPage: true,
+                initialPage: rng.nextInt(images.length),
+                viewportFraction: 1,
+                autoPlay: true,
+              ),
+              itemBuilder: (ctx, index, realIdx) {
+                return Container(
+                  child: images[index],
+                );
+              },
+            )
+          ),
+          SizedBox(height: 20,),
           TextField(
+            focusNode: artistFocusNode,
             onChanged: (text) {
-              artist = text;
+              setState(() {
+                artist = text;
+              });
+            },
+            onEditingComplete: () {
+              node.nextFocus();
             },
             decoration: InputDecoration(
-              hintText: "Artist"
+              labelText: "Artist",
+              labelStyle: TextStyle(
+                fontSize: 20
+              ),
+              suffixIcon: Icon(Icons.music_note_outlined)
             ),
           ),
           SizedBox(height: 20,),
           TextField(
             onChanged: (text) {
-              album = text;
+              setState(() {
+                album = text;  
+              });
+            },
+            onSubmitted: (value) {
+              print(value);
+              if (artist != "") {
+                getReview(artist, album);
+              }
+              else {
+                node.unfocus();
+              }
             },
             decoration: InputDecoration(
-              hintText: "Album"
+              labelText: "Album",
+              labelStyle: TextStyle(
+                fontSize: 20
+              ),
+              suffixIcon: Icon(Icons.library_music_rounded)
             ),
           ),
-          SizedBox(height: 50,),
+          SizedBox(height: 30,),
           ElevatedButton(
-            onPressed: () {
-              getReview(artist, album);
-            },
-            child: Text("Search"),
+            onPressed: album == "" || artist == "" ? null : () => getReview(artist, album),
+            child: Container(width: 100, height: 50, child: Center(child: Text("Search"))),
           )
         ],
         ),
+      );
+       },
       ),
     );
   }
@@ -242,24 +306,26 @@ class _ReviewDetailState extends State<ReviewDetail> {
         itemCount: 1,
         itemBuilder: (BuildContext context, int index) {
         return Padding(
-          padding: const EdgeInsets.all(12.0),
+          padding: const EdgeInsets.all(10.0),
           child: Column(
             children: [
+              SizedBox(height: 10),
               Text(
                 data["artist"],
                 style: TextStyle(
                   fontSize: 20
                 ),
               ),
-              SizedBox(height: 20),
+              SizedBox(height: 10),
               Text(
                 data["album"],
+                textAlign: TextAlign.center,
                 style: TextStyle(
-                  fontSize: 20
+                  fontSize: 35
                 ),
               ),
               SizedBox(height: 20),
-              Image.network(data["cover"]),
+              Image.network(data["cover"], scale: .1,),
               SizedBox(height: 20),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
@@ -330,9 +396,6 @@ class _ExploreState extends State<Explore> {
       setState(() {
         database.value.forEach((key,values) => genres.add(key));   
       });
-      
-
-      print(genres);
   }
 
   void readDataDetail(String genre) async{
@@ -361,6 +424,7 @@ class _ExploreState extends State<Explore> {
     readData();
   }
 
+
   @override
   Widget build(BuildContext context) {
     return genres.length <= 0 ? CircularProgressIndicator() : ListView.builder(
@@ -370,18 +434,26 @@ class _ExploreState extends State<Explore> {
         padding: const EdgeInsets.all(8.0),
         child: Column(
           children: [
-            Text("As more users search for reviews, there will be more to explore here.", style: TextStyle(fontSize: 16), textAlign: TextAlign.center,),
+            SizedBox(height: 10),
+            Text("As more users search for reviews, there will be more to explore here.", 
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 20
+            )),
             SizedBox(height: 20),
             for(var i = 0;i<genres.length; i++) GestureDetector(
               onTap: () {
                 readDataDetail(genres[i]);
               },
                 child: Container(
-                margin: EdgeInsets.all(10),
-                color: Theme.of(context).accentColor,
-                width: MediaQuery.of(context).size.width-50,
-                height: 100,
-                child: Center(child: Text(genres[i], style: TextStyle(fontSize: 20, color: Colors.white)))
+                  margin: EdgeInsets.all(10),
+                  width: MediaQuery.of(context).size.width-50,
+                  height: 100,
+                  decoration: BoxDecoration(
+                    color: Color((Random().nextDouble() * 0xFFFFFF).toInt()).withOpacity(1.0),
+                    borderRadius: BorderRadius.all(Radius.circular(10))
+                  ),
+                  child: Center(child: Text(genres[i], style: TextStyle(fontSize: 25, color: Colors.white)))
               ),
             )
           ],
@@ -408,7 +480,6 @@ class _ExploreDetailState extends State<ExploreDetail> {
   @override
   void initState() { 
     super.initState();
-    print(data["data"]);
   }
 
   void readDataDetail(String band) async{
@@ -452,7 +523,10 @@ class _ExploreDetailState extends State<ExploreDetail> {
               },
                 child: Container(
                 margin: EdgeInsets.all(10),
-                color: Theme.of(context).accentColor,
+                decoration: BoxDecoration(
+                    color: Color((Random().nextDouble() * 0xFFFFFF).toInt()).withOpacity(1.0),
+                    borderRadius: BorderRadius.all(Radius.circular(10))
+                ),
                 width: MediaQuery.of(context).size.width-50,
                 height: 100,
                 child: Center(child: Text(data["data"][i], style: TextStyle(fontSize: 20, color: Colors.white)))
@@ -488,9 +562,6 @@ class _BandState extends State<Band> {
         .child(album)
         .once();
 
-      List <String> albums = [];
-        
-
       Map<String,dynamic> review = {};
       
       setState(() {
@@ -520,7 +591,10 @@ class _BandState extends State<Band> {
               },
                 child: Container(
                 margin: EdgeInsets.all(10),
-                color: Theme.of(context).accentColor,
+                decoration: BoxDecoration(
+                    color: Color((Random().nextDouble() * 0xFFFFFF).toInt()).withOpacity(1.0),
+                    borderRadius: BorderRadius.all(Radius.circular(10))
+                ),
                 width: MediaQuery.of(context).size.width-50,
                 height: 100,
                 child: Center(child: Text(data["data"][i], style: TextStyle(fontSize: 20, color: Colors.white), textAlign: TextAlign.center,))
